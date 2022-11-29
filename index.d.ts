6116e43e430eee0b74bee2ef8517bed8fd1bdb52
Hitesh Kachariya <hiteshkumar.kachariya@101digital.io>
2022-11-29 12:27:14 +0530

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export { default as LoanComponent } from './src';
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const LoanComponent = () => {
+  return (
+    <View>
+      <Text>LoanComponent</Text>
+    </View>
+  );
+};
+
+export default LoanComponent;
